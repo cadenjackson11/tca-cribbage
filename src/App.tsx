d4fb36e0196691cfe2b3e27f5422ee4a811cf659
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-09-18 15:44:01 -0500

    added header button and leaderboard outline

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold mb-3'>Cribbage Companion</h1>
-      <button className='btn btn-circle btn-success'>Play</button>
+    <div className="App p-3">
+      <h1 className='text-3xl font-bold mb-3 text-center'>Cribbage Companion</h1>
+      <button className='btn btn-circle btn-success btn-lg'>Play</button>
+    
+      <div className="card bg-warning text-neutral-content w-96 m-6">
+      
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-error">Leaderboard</h2> 
+      </div>
+</div>
+    
     </div>
+  
+  
+    
   );
 }
 
