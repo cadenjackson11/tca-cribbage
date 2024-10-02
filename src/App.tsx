508928d1006f2827d2bc24fef718df4335635537
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-10-02 14:38:17 -0500

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App p-3">
-      <h1 className='text-3xl font-bold mb-3 text-center'>Cribbage Companion</h1>
-      <button className='btn btn-circle btn-success btn-lg'>Play</button>
+    <div className="App p-3 bg-base-200">
+      <h1 className='text-3xl font-bold mb-3 text-center text-error'>Cribbage Companion</h1>
+      <button className='btn btn-circle btn-accent btn-lg items-center'>Play</button>
     
-      <div className="card bg-warning text-neutral-content w-96 m-6">
+      <div className="card card-outline bg-warning text-neutral-content w-96 m-6">
       
       <div className="card-body items-center text-center">
         <h2 className="card-title text-error">Leaderboard</h2> 
