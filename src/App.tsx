b02b448f4819d7892e5e5d6fc982d9af4c5367bc
cadenjackson11 <143419483+cadenjackson11@users.noreply.github.com>
2024-10-02 14:58:44 -0500

    installed router dom
    npm install react-router-dom
    
    also added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <div>Home</div>
+    },
+
+    {
+      path: "/setup",
+      element: <div>Setup</div>
+
+    },
+
+    {
+      path: "/play",
+      element: <div>Play</div>
+    },
+  ]
+);
 
 function App() {
   return (
-    <div className="App p-3 bg-base-200">
+    <div className="App p-3 ">
+
+
+
+
+
+      <RouterProvider 
+        router={router}
+      />
+
+
+
+
+
       <h1 className='text-3xl font-bold mb-3 text-center text-error'>Cribbage Companion</h1>
-      <button className='btn btn-circle btn-accent btn-lg items-center'>Play</button>
+      <button className='btn btn-circle btn-success btn-lg'>Play</button>
     
       <div className="card card-outline bg-warning text-neutral-content w-96 m-6">
       
